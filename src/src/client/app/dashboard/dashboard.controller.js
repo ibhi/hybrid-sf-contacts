916307ae3efbaded706cdef01517c707a070333f
ibhi <ibrahimin2004@gmail.com>
2016-09-05 10:13:18 +0530

    smartsync.js added as dependency; removed commented code

--- src/src/client/app/dashboard/dashboard.controller.js
+++ src/src/client/app/dashboard/dashboard.controller.js
@@ -5,9 +5,9 @@
     .module('app.dashboard')
     .controller('DashboardController', DashboardController);
 
-  DashboardController.$inject = ['$q', 'dataservice', 'logger', '$cordovaOauth', 'LOGINURL', 'CLIENTID', 'jsforce', 'auth', '$scope'];
+  DashboardController.$inject = ['$q', 'dataservice', 'logger', '$cordovaOauth', 'LOGINURL', 'CLIENTID', 'auth', '$scope'];
   /* @ngInject */
-  function DashboardController($q, dataservice, logger, $cordovaOauth, LOGINURL, CLIENTID, jsforce, auth, $scope) {
+  function DashboardController($q, dataservice, logger, $cordovaOauth, LOGINURL, CLIENTID, auth, $scope) {
     var vm = this;
     document.addEventListener("deviceready", function () {
       vm.news = {
@@ -30,36 +30,13 @@
           console.error('Auth error', error)
         });
         initSmartStore();
-        // var promises = [getMessageCount(), getPeople()];
-        // return $q.all(promises).then(function() {
-        //   logger.info('Activated Dashboard View');
-        // });
+        
       }
-
-      // function getMessageCount() {
-      //   return dataservice.getMessageCount().then(function(data) {
-      //     vm.messageCount = data;
-      //     return vm.messageCount;
-      //   });
-      // }
-
-      // function getPeople() {
-      //   return dataservice.getPeople().then(function(data) {
-      //     vm.people = data;
-      //     return vm.people;
-      //   });
-      // }
 
       var sfSmartStore;
 
       function initSmartStore() {
-        // navigator.smartstore.soupExists('contacts', successCallback, errorCallback);
-
-        // function successCallback() {
-        //   console.log('Contacts soup already exists');
-        // }
-
-        // function errorCallback() {
+        
           var indexSpecs = [
             {
               path: "Id",
@@ -88,7 +65,6 @@
           var errorCallback = function(err) { 
             console.log("registerSoup failed with error:" + err); 
           };
-        // }
 
         sfSmartStore = function () {
           return cordova.require('com.salesforce.plugin.smartstore');
@@ -96,10 +72,6 @@
       }
 
       function queryContacts(accessToken, instanceUrl) {
-        // var conn = jsforce.connection({
-        //   instanceUrl: instanceUrl,
-        //   accessToken: accessToken
-        // });
 
         var query = 'SELECT ' + fieldsList.join(',') + ' from Contact';
 
@@ -113,22 +85,6 @@
           console.error('Error retrieving records ', error);
         })
 
-        // conn.queryPromise(query).then(function(data) {
-        //   console.log(data.records);
-        //   var contacts = data.records;
-        //   if(contacts.length > 0) {
-        //       sfSmartStore().upsertSoupEntries('contacts', contacts, 
-        //         function(items) {
-        //           var statusTxt = "upserted: " + items.length + " contacts";
-        //           console.log(statusTxt);
-            
-        //         }, function(err) {
-        //           console.error('Upsert Error ', err);
-        //         });
-        //   }
-        // }, function(err) {
-        //   console.error('Connection error: ', err);
-        // });
       }
 
       var soupFieldsList = ['Id', 'FirstName', 'LastName', 'Email'];
@@ -168,25 +124,7 @@
       vm.login = function() {
         // queryContacts();
         queryFromSoup();
-        // var accessToken = window.localStorage.getItem('access_token');
-        // var instanceUrl = window.localStorage.getItem('instance_url');
-        // if(accessToken && instanceUrl) {
-        //   queryContacts(accessToken, instanceUrl)
-        // } else {
-
-        //   $cordovaOauth.salesforce(LOGINURL, CLIENTID)
-        //     .then(function(result) {
-        //       console.log(result);
-        //       window.localStorage.setItem('access_token', result.access_token);
-        //       window.localStorage.setItem('refresh_token', result.refresh_token);
-        //       window.localStorage.setItem('instance_url', result.instance_url);
-
-        //       queryContacts(result.access_token, result.instance_url);
-
-        //     }, function(error) {
-        //       console.error(error);
-        //     });
-        // }
+        
       }
 
       vm.logout = function() {
@@ -245,9 +183,6 @@
           }
         );
 
-        // function failureCallback(error) {
-        //   console.log('Error syncing up', error);
-        // }
         cordova.require("com.salesforce.plugin.smartsync")
           .syncUp(soupName, options, successCallback);
       };
